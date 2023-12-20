ab73e324750d95be36615e686e05f2c9b19bf466
saidivyaftr <sdd633@ftr.com>
2023-12-20 20:33:22 +0530

    deleting the poc workflows

--- fuel-app/terraform/nonprodv2/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "default_hostname" {
-  value = module.homepage.default_hostname
-}
-
-output "name" {
-  value = module.homepage.name
-}
--- fuel-app/terraform/nonprodv2/private-endpoint/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "name" {}
-variable "environment" {}
-variable "location" {}
-variable "pvt_endpoint_subnet_id" {}
-variable "resource_group_name" {}
-variable "vnet_id" {}
-variable "private_connection_resource_id" {}
-variable "pvt_dns_zone_id" {}
